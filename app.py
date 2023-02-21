18ba81af31bbb9f501efe7ce98c0a831f65bb92b
shafingit1234 <shafinmohammed315@gmail.com>
2023-02-22 01:44:45 +0530

    review-scrapper-first-commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
-
+import pymongo;
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
@@ -72,6 +72,12 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+            
+            client = pymongo.MongoClient("mongodb+srv://admin_shafin_for_data_science:WVGldyGLHGGqe94O@cluster0.h7x2x29.mongodb.net/?retryWrites=true&w=majority")
+            db = client['review-scrap-database'];
+            review_col = db['review_scrap_data'];
+            review_col.insert_many(reviews);
+            
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
@@ -84,3 +90,5 @@ def index():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
+
+# password : WVGldyGLHGGqe94O
